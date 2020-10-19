b6623cb1f574f9b7d1511b138d22ea20b68db6f5
Cyber Coder <cyberelectro101@gmail.com>
2020-10-18 20:33:37 -0400

    Yea... nevermind, this thing is closed because I'm lazy.

--- contact-me/professional/external-code/professional-contacts.js
+++
@@ -1,2 +0,0 @@
-// Title - Loading Done
-$("title").text("Eagan Farlin's Professional Contacts Page");
--- websites/external-code/list-of-websites.js
+++
@@ -1,2 +0,0 @@
-// Title - Loading Done
-$("title").text("Eagan Farlin's List Of Public Websites");
